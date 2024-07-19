f7001e23b9c727f4e5d32df014bca06a3a7a737f
SergeyKardashev <angeldeep@yandex.ru>
2024-07-19 22:03:06 +0700

    feat: in MenuItem make icons via props, icon padding via props, label via props

--- src/components/App.js
+++ src/components/App.js
@@ -27,8 +27,21 @@ const Menu = ({ children }) => {
 };
 
 // Main item. Can contain sub menu (dropdown)
-const MenuItem = ({ children, url }) => {
-  return <li className="menu-item">{url ? <Link to={url}>{children}</Link> : children}</li>;
+const MenuItem = ({ children, url, icon, text = "item", noText, iconPR, iconPL }) => {
+  const iconClass = `icon ${iconPR ? "icon_pr" : ""} ${iconPL ? "icon_pl" : ""}`;
+
+  return (
+    <li className="menu-item">
+      {(url) ? (
+        <Link to={url}>
+          { icon ? <img src={icon} alt={text} className={iconClass} /> : ''}
+          {children || (noText ? '' : text)} 
+        </Link>
+      ) : (
+        children || (noText ? '' : text )
+      )}
+    </li>
+  );
 };
 
 // Header inside menu. It can live on its onw, but cat has list as a child
@@ -44,7 +57,7 @@ const Dropdown = ({ children }) => {
     <div className="dropdown" onMouseEnter={() => setIsOpen(true)} onMouseLeave={() => setIsOpen(false)}>
       <div className="dropdown-label">
         {children[0]}
-        <img src={dropdownIcon} alt="arrow" style={{marginLeft: 5}} />
+        <img src={dropdownIcon} alt="arrow" style={{ marginLeft: 5 }} />
       </div>
       {isOpen && <div className="dropdown-menu">{children[1]}</div>}
     </div>
@@ -75,21 +88,36 @@ const App = () => {
         <img src={siteLogo} alt="logo" style={{ height: 40, width: 40, marginRight: 10 }} />
         SiteName
       </MenuItem>
+      <MenuItem url="/">Item</MenuItem>
+      <MenuItem text="Icon+text" url="/" icon={starIcon} iconPR></MenuItem>
+      <MenuItem>
+        <Dropdown>
+          Basic menu
+          <DropdownMenu>
+            <DropdownItem url="/sidney">Sidney</DropdownItem>
+            <DropdownItem url="/stockholm">Stockholm</DropdownItem>
+            <DropdownItem url="/new-d elhi">New Delhi</DropdownItem>
+            <DropdownItem url="/beijing">Beijing</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </MenuItem>
       <MenuItem>
         <Dropdown>
           <div style={{ margin: 0, padding: 0, display: "flex", alignItems: "center" }}>
-            Here's submenu 
+            Fancy Dropdown
             <img style={{ height: 20, marginLeft: 8 }} src={logOutIcon} alt="лого" />
           </div>
           <DropdownMenu>
             <MenuHeader>
-              <span style={{ marginRight: 10}}>Cities</span>
-              <button>I'm a button</button>
+              <span style={{ marginRight: 10 }}>Cities</span>
+              <button type="button" style={{ cursor: "pointer" }}>
+                I'm a button
+              </button>
             </MenuHeader>
             <hr />
             <DropdownItem url="/sidney">
               <div style={{ margin: 0, padding: 0, display: "flex", alignItems: "center" }}>
-                <img src={starIcon} alt="star" style={{marginRight: 10}}/>
+                <img src={starIcon} alt="star" style={{ marginRight: 10 }} />
                 Sidney
               </div>
             </DropdownItem>
@@ -97,9 +125,7 @@ const App = () => {
             <DropdownItem url="/new-d elhi">New Delhi</DropdownItem>
             <DropdownItem url="/beijing">Beijing</DropdownItem>
             <hr />
-            <p style={{ margin: "20px 20px" }}>
-              Basic text. Just in case you need it. A comment or cue for user.
-            </p>
+            <p style={{ margin: "20px 20px" }}>Basic text. Just in case you need it. A comment or cue for user.</p>
             <hr />
             <MenuHeader>Parks</MenuHeader>
             <DropdownItem url="/central-park">
@@ -117,47 +143,46 @@ const App = () => {
           </DropdownMenu>
         </Dropdown>
       </MenuItem>
-      <MenuItem url="/bookmark">
-        <img className="bookmark" src={bookmarkIcon} alt="bookmark" />
-        Bookmarks
-      </MenuItem>
-      <MenuItem url="/notifications">
-        <img src={bellIcon} alt="notifications" />
-      </MenuItem>
+      <MenuItem text="Bookmarks" noText url="/bookmark" icon={bookmarkIcon} />
+      <MenuItem text="Notifications" noText url="/notifications" icon={bellIcon}></MenuItem>
       <MenuItem>
         <Dropdown>
           <div style={{ margin: 0, padding: 0, display: "flex", alignItems: "center" }}>
-            <img alt="avatar" src={avatarIcon} style={{ height: 40, marginRight: 8, border: '1px solid red', borderRadius: '50%' }}  />
-            John Doe 
+            <img
+              alt="avatar"
+              src={avatarIcon}
+              style={{ height: 40, marginRight: 8, border: "1px solid gray", borderRadius: "50%" }}
+            />
+            Profile Menu
           </div>
           <DropdownMenu>
             <DropdownItem url="/profile">
               <div style={{ margin: 0, padding: 0, display: "flex", alignItems: "center" }}>
-                <img src={profileProfileIcon} alt="profile" style={{marginRight: 10}}/>
+                <img src={profileProfileIcon} alt="profile" style={{ marginRight: 10 }} />
                 Profile
               </div>
             </DropdownItem>
             <DropdownItem url="/articles-management">
               <div style={{ margin: 0, padding: 0, display: "flex", alignItems: "center" }}>
-                <img src={articlesManageIcon} alt="articles management" style={{marginRight: 10}}/>
+                <img src={articlesManageIcon} alt="articles management" style={{ marginRight: 10 }} />
                 Articles Management
               </div>
             </DropdownItem>
             <DropdownItem url="/feedback">
               <div style={{ margin: 0, padding: 0, display: "flex", alignItems: "center" }}>
-                <img src={feedbackIcon} alt="Feedback" style={{marginRight: 10}}/>
+                <img src={feedbackIcon} alt="Feedback" style={{ marginRight: 10 }} />
                 Feedback
               </div>
             </DropdownItem>
             <DropdownItem url="/settings">
               <div style={{ margin: 0, padding: 0, display: "flex", alignItems: "center" }}>
-                <img src={settingsIcon} alt="settings" style={{marginRight: 10}}/>
+                <img src={settingsIcon} alt="settings" style={{ marginRight: 10 }} />
                 Settings
               </div>
             </DropdownItem>
             <DropdownItem url="/logout">
               <div style={{ margin: 0, padding: 0, display: "flex", alignItems: "center" }}>
-                <img src={logOutIcon} alt="Log out" style={{marginRight: 10}}/>
+                <img src={logOutIcon} alt="Log out" style={{ marginRight: 10 }} />
                 Log out
               </div>
             </DropdownItem>
