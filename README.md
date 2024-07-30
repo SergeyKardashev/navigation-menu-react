2b2601499b7c90c94a295e6345414a7a3d0b224f
SergeyKardashev <angeldeep@yandex.ru>
2024-07-30 16:23:34 +0700

    feat: 1. Add file path to tailwind config; 2. Add Tailwind directives to index css file; 3. Add description about plugins and PostCSS; 4. Clean up index.js from redundant comments

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Navigation menu via React
 
 Educational project to polish my skill of navigation menu making
+
+Run it with command `npm run start`.
+
+To work with Tailwind with intelliSence without warnings from VS Code install extensions `Tailwind CSS IntelliSense` and `PostCSS Language Support`.
+
+There is no need to install PostCSS since the project created via CRA that uses PostCSS implicitly.
--- src/index.js
+++ src/index.js
@@ -9,36 +9,6 @@ import reportWebVitals from "./reportWebVitals";
 
 T.setTexts(translations.ru);
 
-// T.setTexts({
-//   siteName: 'Семейный сайт',
-//   saveButtonText: 'Сохранить',
-//   editButtonText: 'Изменить данные',
-//   site: {
-//     email: 'Lbg@rpri.ru',
-//     phone: '+7 (903) 724-07-97',
-//     teamLead: 'Леонид Жуков'
-//   },
-//   userParams: {
-//     firstName: 'Имя',
-//     lastName: 'Фамилия',
-//     email: 'Почта',
-//   },
-//   nav: {
-//     timeline: 'Лента',
-//     moderation: 'Модерация сайта',
-//     messages: 'Уведомления',
-//     bookmarks: 'Закладки',
-//     otherSites: 'Смежные страницы',
-//     other: 'Другое',
-//     footer: {
-//       home: 'Главная',
-//       aboutProject: 'О проекте',
-//       prices: 'Цены',
-//       feedback: 'Обратная связь',
-//     }
-//   },
-// });
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -48,7 +18,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
